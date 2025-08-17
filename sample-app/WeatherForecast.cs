928ec5eda7d4a2189e904e9df0301dbe18456c5f
Cấn Việt Anh <89600809+vietanh1104@users.noreply.github.com>
2025-08-17 22:19:38 +0700

    update .net version

--- sample-app/WeatherForecast.cs
+++ sample-app/WeatherForecast.cs
@@ -2,7 +2,7 @@ namespace sample_app
 {
     public class WeatherForecast
     {
-        public DateTime Date { get; set; }
+        public DateOnly Date { get; set; }
 
         public int TemperatureC { get; set; }
 
